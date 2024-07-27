c302afb75b88689d80da851a0c9ecd291883b293
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-07-27 14:57:00 -0300

    feat: create task use case

---
+++ src/core/task/useCases/task/create_task.go
@@ -0,0 +1,34 @@
+package task
+
+import (
+	"errors"
+	"time"
+
+	"github.com/jraphaelo/taskmanagement/task/src/core/task/domain/entities"
+	repositories "github.com/jraphaelo/taskmanagement/task/src/core/task/domain/repository"
+)
+
+type CreateTaskUseCaseRequest struct {
+	Title         string `json:"title"`
+	Description   string `json:"description"`
+	PrevisionDate string `json:"prevision_date"`
+}
+
+type CreateTaskUseCase struct {
+	taskRepository repositories.TaskRepository
+}
+
+func (uc *CreateTaskUseCase) Execute(data CreateTaskUseCaseRequest) error {
+	parsedDate, err := time.Parse("2006-01-02", data.PrevisionDate)
+	if err != nil {
+		return errors.New("invalid prevision date format")
+	}
+
+	task, err := entities.NewTask(data.Title, parsedDate, &data.Description)
+	if err != nil {
+		return err
+	}
+
+	err = uc.taskRepository.Save(*task)
+	return err
+}
