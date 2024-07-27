15485d480b97f09b30c9ef9366c9e39189261bb3
JRaphaelO <jraphal@eng.ci.ufpb.br>
2024-07-27 15:17:23 -0300

    test: adjust for use task repository in memory

--- src/core/task/tests/usecases/task/create_task_test.go
+++ src/core/task/tests/usecases/task/create_task_test.go
@@ -4,30 +4,18 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/jraphaelo/taskmanagement/task/src/core/task/domain/entities"
+	"github.com/jraphaelo/taskmanagement/task/src/core/task/infrastructure/database/memory"
 	"github.com/jraphaelo/taskmanagement/task/src/core/task/useCases/task"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-// Mock do repositório de tarefas
-type MockTaskRepository struct {
-	mock.Mock
-}
-
-func (m *MockTaskRepository) Save(task entities.Task) error {
-	args := m.Called(task)
-	return args.Error(0)
-}
-
 func TestCreateTaskUseCase_Execute(t *testing.T) {
-	mockRepo := new(MockTaskRepository)
+	mockRepo := memory.NewInMemoryTaskRepository()
 	uc := &task.CreateTaskUseCase{TaskRepository: mockRepo}
 
 	tests := []struct {
 		name          string
 		request       task.CreateTaskUseCaseRequest
-		mockSaveError error
 		expectedError error
 	}{
 		{
@@ -37,7 +25,6 @@ func TestCreateTaskUseCase_Execute(t *testing.T) {
 				PrevisionDate: "2024-07-27",
 				Description:   "Task description",
 			},
-			mockSaveError: nil,
 			expectedError: nil,
 		},
 		{
@@ -47,7 +34,6 @@ func TestCreateTaskUseCase_Execute(t *testing.T) {
 				PrevisionDate: "invalid-date",
 				Description:   "Task description",
 			},
-			mockSaveError: nil,
 			expectedError: errors.New("invalid prevision date format"),
 		},
 		{
@@ -57,23 +43,14 @@ func TestCreateTaskUseCase_Execute(t *testing.T) {
 				PrevisionDate: "122",
 				Description:   "Task description",
 			},
-			mockSaveError: errors.New("invalid prevision date format"),
 			expectedError: errors.New("invalid prevision date format"),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.mockSaveError != nil {
-				mockRepo.On("Save", mock.AnythingOfType("entities.Task")).Return(tt.mockSaveError)
-			} else {
-				mockRepo.On("Save", mock.AnythingOfType("entities.Task")).Return(nil)
-			}
-
 			err := uc.Execute(tt.request)
-
 			assert.Equal(t, tt.expectedError, err)
-			mockRepo.AssertExpectations(t)
 		})
 	}
 }
